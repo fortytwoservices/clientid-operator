19f495c195af57cbac4d99277f38b67c59f637a7
Ketil Gjerde <477141+mysteq@users.noreply.github.com>
2026-01-20 17:23:06 +0100

    fix: correct naming
    Signed-off-by: Ketil Gjerde <477141+mysteq@users.noreply.github.com>

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -24,7 +24,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	"github.com/deggja/clientid-operator/test/utils"
+	"github.com/fortytwoservices/clientid-operator/test/utils"
 )
 
 const namespace = "clientid-operator-system"
